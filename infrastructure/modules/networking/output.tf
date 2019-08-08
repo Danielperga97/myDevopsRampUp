b7c89207dee87ba094328da1657f64f58d05a767
root <root@localhost.localdomain>
2019-08-08 11:27:28 -0500

    update pipeline

--- infrastructure/modules/networking/output.tf
+++ infrastructure/modules/networking/output.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   value = "${google_compute_network.vpc1.id}"
 }
 output "subnet_id" {
-  value = "${google_compute_subnetwork.subnet1.id}"
+  value = "${google_compute_subnetwork.subnet1.name}"
 }
 output "firewall_id" {
   value = "${google_compute_firewall.firewall.id}"
