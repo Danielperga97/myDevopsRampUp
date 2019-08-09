c6b000bb8ea4faa000628bb7fcc9aec0bab81c94
root <root@localhost.localdomain>
2019-08-09 09:00:02 -0500

    update infraestructure multiple jenkins master

---
+++ infrastructure/modules/computing/jenkins.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "jenkins_instance" {
+  name         = "${var.machine_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "us-east1-b"
+  labels={
+      role = "jenkins"
+  }
+  boot_disk {
+    initialize_params {
+      image = "${var.machine_image}"
+    }
+  }
+  network_interface {
+    subnetwork       = "projects/ramp-up-247818/regions/us-east1/subnetworks/${module.network.subnet_id}"
+    access_config {
+    }
+  }
+  metadata = {
+      ssh-keys= "danielprga:${file("/home/jenkins/google_compute_engine.pub")}"
+  }
+}
