05fd22c774067f449ecd19cbc9de79d9893d4a2e
root <root@localhost.localdomain>
2019-08-13 16:49:43 -0500

    add anchore mods and vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -43,14 +43,7 @@ resource "google_compute_instance" "anchore-instance" {
   metadata = {
       ssh-keys= "danielprga:${file("/home/jenkins/google_compute_engine.pub")}"
   }
-      oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/devstorage.full_control",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append"
-    ]
+
 }
 
 resource "google_compute_instance" "vault-instance" {
@@ -73,14 +66,6 @@ resource "google_compute_instance" "vault-instance" {
   metadata = {
       ssh-keys= "danielprga:${file("/home/jenkins/google_compute_engine.pub")}"
   }
-      oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/devstorage.full_control",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append"
-    ]
 }
 resource "google_container_cluster" "gke-cluster" {
   name               = "gke-cluster-1"
